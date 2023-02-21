f9e9be87234fdf90b9f964a7f93ea652d833c6f5
knackpr <peerawich_r@cmu.ac.th>
2023-02-21 16:04:23 +0700

    งงนิดนึง

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,12 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
+double overlap(Rect R1, Rect R2){
+double rx, ry;
+    rx = min(R1.x+R1.w,R2.x+R2.w) - max(R1.x,R2.x);    
+    ry = min(R1.y,R2.y) - max(R1.y-R1.h,R2.y-R2.h);
+    
+    if(rx < 0 || ry < 0) return 0;
+    else return rx * ry;
 
 }
